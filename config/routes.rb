8882994a9943ae7c2095bbf6cb96d30d849174fd
Bhanu Pratap <bhanu.chauhan@greatlearning.in>
2021-09-08 12:10:24 +0530

    Add home page for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root "sessions#new"
+  resources :sessions, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
