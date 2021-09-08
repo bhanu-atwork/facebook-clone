f8bfb1061ec4dbcebc455da4618ee6e9c1ab0bc4
Bhanu Pratap <bhanu.chauhan@greatlearning.in>
2021-09-08 19:51:42 +0530

    Update session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,18 @@
 class SessionsController < ApplicationController
   skip_before_action :require_login, only: [:new, :create]
+  before_action :get_user, only: [:create]
   
   def new
   end
 
   def create
-    session_params = params.permit(:email, :password)
-    @user = User.find_by(email: session_params[:email])
-    if !@user
-      flash[:email_notice] = "Your user is not connected to facebook"
+    if @user.password.eql?(session_params[:password])
+      session[:user_id] = @user.id
+      return redirect_to @user
     else
-      if @user.password.eql?(session_params[:password])
-        session[:user_id] = @user.id
-        return redirect_to @user
-      else
-        flash[:password_notice] = "Please enter correct password"
-      end
+      flash[:password_notice] = "Please enter correct password"
+      redirect_to new_session_path
     end
-    redirect_to new_session_path
   end
 
   def destroy
@@ -25,4 +20,17 @@ class SessionsController < ApplicationController
     flash[:notice] = "You have been signed out!"
     redirect_to new_session_path
   end
+
+  def get_user
+    @user = User.find_by(email: session_params[:email])
+    if !@user
+      flash[:email_notice] = "Your user is not connected to facebook"
+      redirect_to new_session_path
+    end
+  end
+
+  private
+    def session_params
+      params.permit(:email, :password)
+    end
 end
