3ca1bf2697690c3851f3847f0c876c6614206414
Bhanu Pratap <bhanu.chauhan@greatlearning.in>
2021-09-12 22:10:10 +0530

    Add friend functionality

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,9 +1,25 @@
 class FriendsController < ApplicationController
+  
   def show
-    @friend_requests = Friend.where.not(first_user_id: :id)
+    @current_user = helpers.current_user
+    @friend_requests = Friend.where(second_user_id: @current_user.id, status: 2)
   end
 
   def new
+    @current_user = helpers.current_user
+    @friend_request = Friend.new(first_user_id: @current_user.id, second_user_id: params["friend_id"], status: 2)
+    if !@friend_request.save
+      flash[:errors] = @friend_request.errors.full_messages
+    end
+    redirect_to friend_path(@current_user.id)
   end
 
+  def create
+    @current_user = helpers.current_user
+    @friend_status = Friend.new(:status => params["status"].to_i)
+    if !@friend_status.save
+      flash[:errors] = @friend_status.errors.full_messages
+    end
+    redirect_to friend_path(@current_user.id)
+  end
 end
--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,5 +1,6 @@
 class Friend < ApplicationRecord
   belongs_to :user, :class_name => 'User', :foreign_key => 'first_user_id'
   belongs_to :user, :class_name => 'User', :foreign_key => 'second_user_id'
-  enum status: { active: 0, blocked: 1, inactive: 2 }
+  enum status: { accept: 0, reject: 1, block: 2, remove:3}
+  validates_uniqueness_of :first_user_id, scope: :second_user_id
 end
