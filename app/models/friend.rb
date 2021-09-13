a9470d2692620721ebce56922b16ef1f8b42ae7d
Bhanu Pratap <bhanu.chauhan@greatlearning.in>
2021-09-13 09:40:42 +0530

    Remove redundant file

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -2,5 +2,5 @@ class Friend < ApplicationRecord
   belongs_to :user, :class_name => 'User', :foreign_key => 'first_user_id'
   belongs_to :user, :class_name => 'User', :foreign_key => 'second_user_id'
   enum status: { accepted: 0, rejected: 1, blocked: 2, removed:3}
-  # validates_uniqueness_of :first_user_id, scope: :second_user_id
+  validates_uniqueness_of :first_user_id, scope: :second_user_id
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :image, dependent: :destroy
   has_many :comments
-  has_many :likes, :as => :likeable
+  has_many :likes, as: :likeable
 end
