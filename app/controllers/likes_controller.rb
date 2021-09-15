6e2a98f93be30852c60efc41107f8defb5229af4
Bhanu Pratap <bhanu.chauhan@greatlearning.in>
2021-09-15 14:46:45 +0530

    Correct like controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,28 @@ class LikesController < ApplicationController
 
   def create
     @like = @current_user.likes.build(likeable_type: params["type"], likeable_id: params["id"])
-    flash[:errors] = @like.errors.full_messages unless @like.save
-    redirect_to user_path(@current_user)
+    payload = {"error": nil}
+    status_code = :ok
+    unless @like.save
+      payload["error"] = @like.errors.full_messages
+      status_code = :internal_server_error 
+    end
+    payload["current_likes_count"] = Like.where(likeable_type: params["type"], likeable_id: params["id"]).count
+    render json: payload, status: status_code
   end 
 
   def destroy
     like_condition_map = { user_id: @current_user.id, likeable_type: params["type"], likeable_id: params["id"] }
-    Like.destroy_by(like_condition_map)
+    @like = Like.find_by(like_condition_map)
+    payload = {"error": nil}
+    status_code = :ok
+    if @like.nil?
+      payload["error"] = "No Data Like Found"
+      status_code = :internal_server_error 
+    else
+      @like.destroy
+    end
+    payload["current_likes_count"] = Like.where(likeable_type: params["type"], likeable_id: params["id"]).count
+    render json: payload, status: status_code
   end
 end
