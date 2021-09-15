48479884251721ddff1778c9718f81dda31106ae
Bhanu Pratap <bhanu.chauhan@greatlearning.in>
2021-09-15 23:42:59 +0530

    Refactor as per multi user

--- app/javascript/packs/users.js
+++ app/javascript/packs/users.js
@@ -1,10 +1,14 @@
 var like_post_comment;
-$(document).ready(function () {
-  $("#1").hide();
+var show_hide_comment;
 
-  $("#xta").click(function () {
-    $("#1").toggle();
-  });
+$(document).ready(function () {
+  $("#comment-block").hide();
+
+  window.show_hide_comment = function (element) {
+    id = $(element).attr("id_value");
+    id_val = "[id_value=" + id + "]";
+    $("div").filter("[id='comment-block']").filter(id_val).toggle();
+  };
 
   window.like_post_comment = function (event, element) {
     event.preventDefault();
@@ -19,12 +23,17 @@ $(document).ready(function () {
         data: { type: type, id: id },
         success: function (result) {
           console.log(result);
-
+          id_val = "[id_value=" + id + "]";
           if (type == "Post") {
-            $("#post_like_icon").text(result["current_likes_count"]);
-            $("#post_like").attr("action", "unlike");
+            $("i")
+              .filter("[id='post_like']")
+              .filter(id_val)
+              .attr("action", "unlike");
+            $("span")
+              .filter("[id='post_like_icon']")
+              .filter(id_val)
+              .text(result["current_likes_count"]);
           } else {
-            id_val = "[id_value=" + id + "]";
             $("i")
               .filter("[id='comment_like']")
               .filter(id_val)
@@ -46,11 +55,17 @@ $(document).ready(function () {
         data: { type: type, id: id },
         success: function (result) {
           console.log(result);
+          id_val = "[id_value=" + id + "]";
           if (type == "Post") {
-            $("#post_like_icon").text(result["current_likes_count"]);
-            $("#post_like").attr("action", "like");
+            $("i")
+              .filter("[id='post_like']")
+              .filter(id_val)
+              .attr("action", "like");
+            $("span")
+              .filter("[id='post_like_icon']")
+              .filter(id_val)
+              .text(result["current_likes_count"]);
           } else {
-            id_val = "[id_value=" + id + "]";
             $("i")
               .filter("[id='comment_like']")
               .filter(id_val)
@@ -67,4 +82,5 @@ $(document).ready(function () {
       });
     }
   };
+
 });
