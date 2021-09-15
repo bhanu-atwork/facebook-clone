37fb73e90e5387e5635d3ff1cfcd8a6345d6a677
Bhanu Pratap <bhanu.chauhan@greatlearning.in>
2021-09-15 12:16:44 +0530

    Add friend list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   
   def show
     @current_users_posts = @current_user.posts
+    @friends = Friend.where(second_user_id: @current_user.id , status: 0)
   end
 end
